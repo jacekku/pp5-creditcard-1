40cdc1e8cfe882d5a3297fe126ed70558cce6eff
s-115-30 <s-115-30@gumiforest.ae.krakow.pl>
2019-10-30 08:39:42 +0100

    add test

--- src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/creditcard/model/CreditCardTest.java
@@ -18,4 +18,17 @@ public class CreditCardTest {
         //Assert / Then
         Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(LIMIT)));
     }
+    @Test
+    public void creditBelowGeneralLimitIsNotPossible() {
+        //Arrange
+        CreditCard card = new CreditCard("1234-5678");
+        //Act
+        //Assert
+        try {
+            card.assignLimit(BigDecimal.valueOf(50));
+            Assert.fail("exception");
+        } catch (CreditBelowLimitException e) {
+            Assert.assertTrue(true);
+        }
+    }
 }
